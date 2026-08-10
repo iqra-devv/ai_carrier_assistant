eef4e3c6c92af78072291c9db22507cfed42918b
iqra-devv <mailto.iqraliaqat@gmail.com>
2026-08-10 03:37:37 -0700

    feat: add JWT authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path 
 from .views import RegisterView
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
 
 # Defines the URL pattern for the user registration API.
 # When a POST request is sent to "register/", it calls RegisterView
 # to validate the registration data and create a new user.
 urlpatterns = [
     path ("register/", RegisterView.as_view(), name= "register" ),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
