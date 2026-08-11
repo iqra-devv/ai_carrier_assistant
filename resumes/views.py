563a341196304f3352d01b8ab384bf2dca32254c
iqra-devv <mailto.iqraliaqat@gmail.com>
2026-08-11 06:19:37 -0700

    feat: add authenticated resume upload endpoint

--- resumes/views.py
+++ resumes/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.generics import CreateAPIView
+from .models import Resume
 from .serializers import ResumeSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
 # Create your views here.
 
-class ResumeUploadView(APIView):
+class ResumeUploadView(CreateAPIView):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
     permission_classes = [IsAuthenticated]
 
-    def post(self,request):
-        serializer = ResumeSerializer(data = request.data)
-        if serializer.is_valid():
-            resume = serializer.save(user = request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
-            return Response(
-                ResumeSerializer(resume).data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
-        
+  
